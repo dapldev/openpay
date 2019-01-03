9defe02c4d19a3fb7f11a295c25dc3dcc4195c7b
ritayan <rityan@digitalaptech.com>
2019-01-03 14:52:21 +0530

    composer version

--- failure.php
+++
@@ -1,8 +0,0 @@
-<?php
-print_r($_GET);
-//Something to write to txt log
-$log  = "Call-time: ".$_SERVER['REMOTE_ADDR'].' - '.date("F j, Y, g:i a").PHP_EOL.
-         "Log: ".$_GET.PHP_EOL.
-        "-------------------------".PHP_EOL;
-//Save string to log, use FILE_APPEND to append.
-file_put_contents('./lib/Openpay/Log/log'.date("j.n.Y").'.log', $log, FILE_APPEND);
